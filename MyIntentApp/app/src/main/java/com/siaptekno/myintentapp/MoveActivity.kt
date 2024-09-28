be4ea30831d7a4696bf5f1667645d6776b62e491
haikal <asmanhaikal@gmail.com>
2024-09-28 22:12:54 +0700

    feat: add button to move to another activity

--- MyIntentApp/app/src/main/java/com/siaptekno/myintentapp/MoveActivity.kt
+++ MyIntentApp/app/src/main/java/com/siaptekno/myintentapp/MoveActivity.kt
@@ -1,33 +1,20 @@
 package com.siaptekno.myintentapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MoveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_move)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val moveActivityButton = findViewById<Button>(R.id.moveActivityButton)
-        moveActivityButton.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.moveActivityButton -> {
-                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
-                startActivity(moveIntent)
-            }
         }
     }
 }
