b873506eb8c2cc3ed393180a4143bbc2ef970032
haikal <asmanhaikal@gmail.com>
2024-09-30 18:27:53 +0700

    feat: add implicit intent-dial number feature

--- MyIntentApp/app/src/main/java/com/siaptekno/myintentapp/MainActivity.kt
+++ MyIntentApp/app/src/main/java/com/siaptekno/myintentapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.siaptekno.myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         moveActivityWithDataButton.setOnClickListener(this)
         val moveActivityObjectButton = findViewById<Button>(R.id.moveActivityObjectButton)
         moveActivityObjectButton.setOnClickListener(this)
+        val dialNumberButton = findViewById<Button>(R.id.dialNumberButton)
+        dialNumberButton.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -50,6 +53,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveWithObjectActivity = Intent(this@MainActivity, MoveWithObjectActivy::class.java)
                 moveWithObjectActivity.putExtra(MoveWithObjectActivy.EXTRA_PERSON, person)
                 startActivity(moveWithObjectActivity)
+            }
+            R.id.dialNumberButton -> {
+                val phoneNumber = "081210841382"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
             }
         }
     }
